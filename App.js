c380466564dded94dfaf583d1f9d8599048e0cc7
SSanjay21 <103240670+SSanjay18@users.noreply.github.com>
2024-08-29 12:47:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import LoginRegister from "./LoginRegister/LoginRegister";
+
+
+function App (){
+  return (
+    <div>
+      <LoginRegister />
+    </div>
+  );
+  
+}
+
+export default App;
